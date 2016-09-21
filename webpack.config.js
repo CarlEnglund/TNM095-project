78fd8b7d3516be460a714dcf0aec53ed06db063d
Klas Eskilson <klas.eskilson@gmail.com>
2016-09-21 14:47:13 +0200

    Add npm package and webpack config

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+    entry: "./app/index.js",
+    output: {
+        path: __dirname + "/dist",
+        filename: "bundle.js"
+    },
+    module: {
+        loaders: [
+            { test: /\.css$/, loader: "style!css" },
+            { test: /\.scss$/, loaders: ["style", "css", "sass"] },
+            { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader"},
+            { test: /\.glsl$/, loader: 'webpack-glsl' }
+        ]
+    },
+    plugins: [],
+    resolve: {
+        extensions: ['', '.js', '.jsx']
+    }
+};
