e21d2a8d2028a31a99f46c80821d1622c1f421dc
Carl Englund <englund92@gmail.com>
2016-09-28 11:22:38 +0200

    Updated Nest and Resource classes
    - Added update functions to both
    - Nest can now grow and shrink

--- app/Nest.js
+++ app/Nest.js
@@ -1,10 +1,41 @@
 class Nest {
-  constructor(position) {
+  constructor(world, position, width, height, resources) {
     this.postion = position;
+    this.width = width;
+    this.height = height;
+    this.resources = resources;
+    this.ctx = world.context;
+  }
+  
+  update() {
+    this.ctx.save();
+    this.ctx.translate(this.position.x, this.position.y);
+    this.ctx.fillStyle = "Red";
+    this.ctx.fillRect(this.positon.x, this.position.y, this.width, this.height);
+    this.ctx.restore();
   }
 
   getPostion() {
     return this.position;
   }
 
+  growNest() {
+    this.width++;
+    this.height++;
+  }
+  
+  shrinkNest() {
+    this.width--;
+    this.height--;
+  }
+
+  addResourceToNest() {
+    this.resources++;
+  }
+
+  removeResourceFromNest() {
+    this.resources--;
+  }
 }
+export default Nest;
+
--- app/Resource.js
+++ app/Resource.js
@@ -1,11 +1,22 @@
 class Resource {
-  constructor(position, level) {
+  constructor(world, position, lifetime, level) {
     this.position = position;
     this.resourceLevel = level
+    this.lifetime = lifetime;
+    this.ctx = world.context;
+  }
+  
+  update() {
+    this.ctx.save();
+    this.ctx.translate(this.position.x, this.position.y);
+    this.ctx.fillStyle = "Blue";
+    this.ctx.fillRect(this.postion.x, this.postion.y, 20, 20);
+    this.ctx.restore();
   }
 
   getPosition() {
    return this.position;
   }
-};
+}
+export default Resource;
 
