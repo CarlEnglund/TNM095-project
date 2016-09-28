01038cb4ac097aab819d5e4fb95c017923938502
Klas Eskilson <klas.eskilson@gmail.com>
2016-09-28 10:56:40 +0200

    Add support for default exports

--- webpack.config.js
+++ webpack.config.js
@@ -2,18 +2,17 @@ module.exports = {
     entry: "./app/index.js",
     output: {
         path: __dirname + "/dist",
-        filename: "bundle.js"
+        filename: "bundle.js",
     },
     module: {
         loaders: [
             { test: /\.css$/, loader: "style!css" },
             { test: /\.scss$/, loaders: ["style", "css", "sass"] },
             { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader"},
-            { test: /\.glsl$/, loader: 'webpack-glsl' }
-        ]
+        ],
     },
     plugins: [],
     resolve: {
-        extensions: ['', '.js', '.jsx']
-    }
+        extensions: ['', '.js']
+    },
 };
