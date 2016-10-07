d37409ef86407d188d3286426b1c5c38e16a714d
Carl Englund <englund92@gmail.com>
2016-10-07 15:42:32 +0200

    Consume some resources continously

--- app/Nest.js
+++ app/Nest.js
@@ -10,22 +10,23 @@ class Nest {
   }
 
   update(world) {
-    if (this.width > 0 && this.height > 0) {
+    if (this.width > 0 && this.height > 0 && this.resources.length) {
       this.shrinkNest();
     }
     if (this.resources.length) {
-      setTimeout(this.createBots.bind(world), 5000);
+      setTimeout(this.createBots.bind(world, this), 5000);
     }
+    console.log(this.resources.length)
   }
 
   get style() {
     return 'red';
   }
-  createBots() {
-    const botPos = this.nests[0].position.Copy();
-    botPos.add(Vec.Random({minX: -3, minY: -3, maxX: 3, maxY: 3}));
-    this.createBots(botPos, this.nests[0].position, this.nests[0]);
-    this.nests[0].resources.pop();
+
+  createBots(nest) {
+    console.log(nest)
+    this.createBots(nest.position, nest);
+    nest.resources.pop();
     // Hacky way to clear timeouts, see http://stackoverflow.com/questions/3141064/how-to-stop-all-timeouts-and-intervals-using-javascript
     // Third answer.
     console.log('Created bot')
@@ -46,6 +47,7 @@ class Nest {
   shrinkNest() {
     this.width -= (1/60);
     this.height -= (1/60);
+    setTimeout(this.removeResourceFromNest.bind(this), 5000);
   }
 
   addResourceToNest() {
@@ -53,7 +55,8 @@ class Nest {
   }
 
   removeResourceFromNest() {
-    this.resources--;
+    console.log(this)
+    this.resources.pop();
   }
 
   addResource(resource) {
